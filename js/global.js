41768435d2e84fc3fe5c231cc3a12456ed7dbe27
borga <borgaaydin@gmail.com>
2017-03-15 01:53:53 +0100

    lol

--- js/global.js
+++ js/global.js
@@ -2,13 +2,23 @@
  * Created by borga on 28/02/17.
  */
 
+
 $(function() {
     $(".main_slider").slick({
         centerMode: true,
         centerPadding: '20px',
         slidesToShow: 3,
         arrows: true,
-        variableWidth: true
+        variableWidth: true,
+        dots: true
+
+    });
+
+    $(".category_slider").slick({
+
+        slidesToShow: 1,
+        arrows: true,
+        dots: true
 
     });
 
@@ -30,12 +40,22 @@ $(function() {
         slidesToScroll: 1,
         asNavFor: '.slider-for',
         dots: true,
-        focusOnSelect: true
+        focusOnSelect: true,
+        vertical: true
     });
 
     $('.category-button').mouseenter(function(){
         $('.overlay').toggle();
     }).mouseleave(function() {
         $('.overlay').toggle();
+    });
+
+    $('.inputFields input').blur(function()
+    {
+        if( $(this).val() ) {
+            $(this).addClass("not-empty")
+        } else {
+            $(this).removeClass("not-empty");
+        }
     });
 });
